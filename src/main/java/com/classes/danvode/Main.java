ee06e275225cda4b86dafda923f926249bd73573
A A <dsa>
2021-10-02 14:21:36 +0300

    Initial commit

---
+++ src/main/java/com/classes/danvode/Main.java
@@ -0,0 +1,27 @@
+package main.java.com.classes.danvode;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args){
+        int[] array = {4, 2,6, 9,1,7};
+        boolean a = true;
+        while (a) {
+            a = false;
+            for (int i = 1; i < array.length; i++) {
+                if (array[i] < array[i - 1]) {
+                    swap(array, i, i-1);
+                    a = true;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(array));
+
+    }
+
+    private static void swap(int[] array, int b1, int b2) {
+        int tmp = array[b1];
+        array[b1] = array[b2];
+        array[b2] = tmp;
+    }
+}
